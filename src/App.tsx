06bdb2ce3fab3af2bca8c498e1f4691ed47534f9
iduk <iduk.design@gmail.com>
2025-08-03 16:32:07 +0900

    Sentry 추가.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import { RouterProvider } from "react-router-dom"
 import routerComponent from "@/routes"
+import * as Sentry from "@sentry/react"
+
+Sentry.init({
+  dsn: "https://2229882f92834d9ea40999fcab5af966@o403043.ingest.us.sentry.io/5265121",
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
+})
 
 function App() {
   const router = routerComponent()
   return (
     <>
       <RouterProvider router={router} />
+
+      {process.env.NODE_ENV === "development" && (
+        <button
+          className="absolute bottom-4 right-4 bg-amber-600 hover:bg-amber-700 active:bg-amber-800 text-white px-3 py-2 rounded"
+          onClick={() => {
+            throw new Error("테스트 에러 발생")
+          }}
+        >
+          Sentry Test
+        </button>
+      )}
     </>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin"
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "vite"
@@ -5,13 +6,26 @@ import react from "@vitejs/plugin-react"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    sentryVitePlugin({
+      org: "iduk",
+      project: "iduk",
+    }),
+  ],
+
   server: {
     hmr: true, // 수정된 부분만 빠르게 반영 (Hot Module Replacement)
   },
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+
+  build: {
+    sourcemap: true,
   },
 })
